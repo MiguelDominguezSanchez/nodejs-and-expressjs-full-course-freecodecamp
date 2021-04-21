edf97c04452eb000de95fcba39f80723900d8f04
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-21 12:35:04 +0200

    Os Module

--- app.js
+++ app.js
@@ -21,6 +21,7 @@
 // 	console.log('hello world')
 // }, 1000)
 
+/*
 // CommonJS, every file is module (by  default)
 // Modules - Encapsulated code (only share minimum)
 const names = require('./4-names')
@@ -32,3 +33,24 @@ require('./7-mind-grenade')
 // sayHi('susan')
 // sayHi(names.john)
 // sayHi(names.peter)
+*/
+
+/*min 5:49*/
+
+/////////////////////////////////////////////////////////////////
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// method retuns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+	name: os.type(),
+	release: os.release(),
+	totalMen: os.totalmem(),
+	freeMen: os.freemem(),
+}
+console.log(currentOS)
