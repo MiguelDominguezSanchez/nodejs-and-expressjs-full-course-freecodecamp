50d3b6c7a2e7097349e29328ab27b4d36ff95788
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-21 11:43:21 +0200

    First Module

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     – info about current moduule (file)
+// process    - info abouut env where the program is being executed
+
+console.log(__dirname)
+setInterval(() => {
+	console.log('hello world')
+}, 1000)
---
+++ app.js
@@ -0,0 +1,32 @@
+// const amount = 12
+
+// if (amount < 10) {
+// 	console.log('small number')
+// } else {
+// 	console.log('large number')
+// }
+
+// console.log(`hey it´s my first node app!!`)
+
+// GLOBALS - NO WINDOW
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     – info about current moduule (file)
+// process    - info abouut env where the program is being executed
+
+// console.log(__dirname)
+// setInterval(() => {
+// 	console.log('hello world')
+// }, 1000)
+
+// CommonJS, every file is module (by  default)
+// Modules - Encapsulated code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+// console.log(names)
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
