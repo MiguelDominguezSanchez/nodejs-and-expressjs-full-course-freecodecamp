616881cf4127cc1ef688a8dbf8571837a2eb274b
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-21 12:11:11 +0200

    Alternative Syntax

--- app.js
+++ app.js
@@ -26,7 +26,8 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils')
 // console.log(names)
-
+const data = require('./6-alternative-flavor')
+console.log(data)
 sayHi('susan')
-sayHi(john)
-sayHi(peter)
+sayHi(names.john)
+sayHi(names.peter)
