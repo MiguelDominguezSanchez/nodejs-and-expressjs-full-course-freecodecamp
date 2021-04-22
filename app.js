d25cf91e28f345bc69e17e5b761e1f30a0f81133
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-22 18:05:31 +0200

    Http Rrrequest object

--- app.js
+++ app.js
@@ -413,10 +413,25 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
 	// console.log('user hit the server')
-	res.writeHead(200, { 'content-type': 'text/html' })
-	// res.end('home page')
-	res.write('<h1>home page</h1>')
-	res.end()
+	// console.log(req.method)
+	// console.log(req.url)
+	const url = req.url
+	// home
+	if (url === '/') {
+		res.writeHead(200, { 'content-type': 'text/html' })
+		// res.end('home page')
+		res.write('<h1>home page</h1>')
+		res.end()
+	} // about page
+	else if (url === '/about') {
+		res.writeHead(200, { 'content-type': 'text/html' })
+		res.write('<h1>about page</h1>')
+		res.end()
+	} else {
+		res.writeHead(404, { 'content-type': 'text/html' })
+		res.write('<h1>page not found</h1>')
+		res.end()
+	}
 })
 
 server.listen(5000)
