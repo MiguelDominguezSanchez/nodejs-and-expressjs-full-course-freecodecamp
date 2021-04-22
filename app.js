8280360495930092e18e99160549bbcae6a627cb
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-22 17:31:08 +0200

    Http - Headers

--- app.js
+++ app.js
@@ -412,8 +412,11 @@ http
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-	console.log('user hit the server')
-	res.end('home page')
+	// console.log('user hit the server')
+	res.writeHead(200, { 'content-type': 'text/html' })
+	// res.end('home page')
+	res.write('<h1>home page</h1>')
+	res.end()
 })
 
 server.listen(5000)
